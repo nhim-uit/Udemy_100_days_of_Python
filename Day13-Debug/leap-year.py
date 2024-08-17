1f40bef0f7c64336a1b203308a31d902e3f6a45b
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-08-17 19:26:10 +0700

    debug

---
+++ Day13-Debug/leap-year.py
@@ -0,0 +1,17 @@
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+
+print(is_leap(2000))  # leap year
+print(is_leap(2012))  # leap year
+print(is_leap(2013))  # not leap year
+print(is_leap(1900))  # not leap year
