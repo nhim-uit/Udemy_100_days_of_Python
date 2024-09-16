d3c7f3cfe3ff65e7d9b6c8741d828f1919cdfad4
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-09-16 19:13:14 +0700

    refactor paddle

--- Day22-Pong/paddle.py
+++ Day22-Pong/paddle.py
@@ -4,36 +4,31 @@ from CONSTANTS import *
 
 class Paddle:
     def __init__(self, side, direction):
-        self.__paddle = []
-        self.create(side)
-        self.__head = self.__paddle[0]
-        self.__head.setheading(direction)
+        self.__paddle = MyTurtle(side, 0)
+        self.__paddle.setheading(direction)
 
-    def create(self, start_x=0, size=PIXEL):
-        self.__paddle.append(MyTurtle(start_x, 0))
-
-    def get_head(self):
-        return self.__head
+    def get_paddle(self):
+        return self.__paddle
 
     def move(self):
         # Move forward by 1 pixel
-        self.__head.forward(MOVE_DISTANCE)
+        self.__paddle.forward(MOVE_DISTANCE)
 
     def auto_move(self):
-        if self.__head.ycor() > EDGE:
-            self.__head.setheading(DOWN)
-        if self.__head.ycor() < -EDGE + PIXEL:
-            self.__head.setheading(UP)
+        if self.__paddle.ycor() > EDGE:
+            self.__paddle.setheading(DOWN)
+        if self.__paddle.ycor() < -EDGE + PIXEL:
+            self.__paddle.setheading(UP)
         self.move()
 
     def up(self):
-        if self.__head.ycor() <= EDGE:
-            self.__head.setheading(UP)
+        if self.__paddle.ycor() <= EDGE:
+            self.__paddle.setheading(UP)
             self.move()
 
     def down(self):
-        if self.__head.ycor() >= -EDGE + PIXEL:
-            self.__head.setheading(DOWN)
+        if self.__paddle.ycor() >= -EDGE + PIXEL:
+            self.__paddle.setheading(DOWN)
             self.move()
 
 
