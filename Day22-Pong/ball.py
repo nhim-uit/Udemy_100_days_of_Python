9c6b22a91d7758324fa6d75fe5ada5f9de088fdd
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-09-16 18:57:59 +0700

    add ball moving speed

--- Day22-Pong/ball.py
+++ Day22-Pong/ball.py
@@ -1,7 +1,9 @@
-from turtle import Turtle
-from CONSTANTS import *
 from random import randint
+from turtle import Turtle
+
+from CONSTANTS import *
 from paddle import Paddle
+from scoreboard import ScoreBoard
 
 
 class Ball(Turtle):
@@ -15,6 +17,7 @@ class Ball(Turtle):
         self.setheading(randint(30, 60))
         self.x_move = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
         # move
@@ -24,31 +27,35 @@ class Ball(Turtle):
         # move forward
         self.goto(new_x, new_y)
 
-    def detect_wall(self):
-        """
-        Detect collision with wall
-        :return: False if collides with right or left wall
-        """
-        if self.xcor() > WALL_WIDTH \
-                or self.xcor() < -WALL_WIDTH:
+    def detect_wall(self, scoreboard: ScoreBoard):
+        # detect right boundary
+        if self.xcor() > WALL_WIDTH:
+            scoreboard.l_point()
             self.goto(0, 0)
             self.bounce_x()
+            self.move_speed = 0.1
+
+        # detect left boundary
+        if self.xcor() < -WALL_WIDTH:
+            scoreboard.r_point()
+            self.goto(0, 0)
+            self.bounce_x()
+            self.move_speed = 0.1
+
+        # detect top and bottom
         if self.ycor() > WALL_HEIGHT \
                 or self.ycor() < -WALL_HEIGHT:
             self.bounce_y()
 
-        return True
-
     def bounce_x(self):
-        # self.setheading(randint(75, 135) + self.heading())
         self.x_move *= -1
+        self.move_speed *= 0.9
 
     def bounce_y(self):
-        # self.setheading(randint(135, 180) + self.heading())
         self.y_move *= -1
 
     def detect_paddle(self, paddle: Paddle):
-        if self.distance(paddle.get_head()) < 50 and self.xcor() > WALL_WIDTH - PIXEL \
-                or self.distance(paddle.get_head()) < 50 and self.xcor() < - WALL_WIDTH + PIXEL:
+        if self.distance(paddle.get_head()) < 50 and self.xcor() > WALL_WIDTH - 2 * PIXEL \
+                or self.distance(paddle.get_head()) < 50 and self.xcor() < - WALL_WIDTH + 2 * PIXEL:
             self.bounce_x()
 
