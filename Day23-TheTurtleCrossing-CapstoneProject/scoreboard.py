fc57f45413fcb7672f03919211fe76f1283b5f24
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-09-19 11:35:49 +0700

    add scoreboard, gameover

--- Day23-TheTurtleCrossing-CapstoneProject/scoreboard.py
+++ Day23-TheTurtleCrossing-CapstoneProject/scoreboard.py
@@ -1,5 +1,21 @@
 from turtle import Turtle
+from CONSTANTS import *
 
 
 class Scoreboard(Turtle):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.color('black')
+        self.setposition(-EDGE, EDGE - PIXEL)
+        self.level = 1
+        self.write(f'Level: {self.level}',
+                   move=False,
+                   align='left',
+                   font=FONT)
+
+    def game_over(self):
+        self.clear()
+        self.goto(0, 0)
+        self.write('GAME OVER', move=False, align='center', font=FONT)
