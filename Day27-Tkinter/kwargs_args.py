0dfc24193b474e29fcaa709b4acb529d64203813
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-02 17:58:38 +0700

    kwargs examples

--- Day27-Tkinter/kwargs_args.py
+++ Day27-Tkinter/kwargs_args.py
@@ -12,6 +12,42 @@ def add(*args):
     return s
 
 
+def calculate(n, **kwargs):
+    print(kwargs)
+
+    # kwargs is dictionary
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+
+    # print(kwargs['add'])
+    # n += kwargs['add']
+    # n *= kwargs['multiply']
+    # this will return error if no key word is found
+
+    # use this instead
+    n1 = kwargs.get('add')
+    n2 = kwargs.get('multiply')
+    n3 = kwargs.get('divide')
+    n4 = kwargs.get('subtract')
+
+    if n1:
+        n += n1
+
+    if n2:
+        n *= n2
+
+    if n3:
+        n /= n3
+
+    if n4:
+        n -= n4
+
+    return n
+
+
 if __name__ == '__main__':
     print(add(1, 2, 5, 7, 9))
     print(add(1, 2, 3))
+
+    print(calculate(5, add=3, multiply=5))
