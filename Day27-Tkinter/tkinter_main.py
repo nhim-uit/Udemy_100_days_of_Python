1e72a4780431084bc93e25057c64e5fb83a7d9b8
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-04 14:38:34 +0700

    label, button, entry

--- Day27-Tkinter/tkinter_main.py
+++ Day27-Tkinter/tkinter_main.py
@@ -2,18 +2,38 @@
 # Oct 2nd, 2024
 # Day 27 - Tkinter
 
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title('My First GUI Program')
 window.minsize(width=500, height=300)
 
 # label
-label = tkinter.Label(text='I Am a Label', font=('Arial', 24, 'bold'))
-label.pack(expand=True)  # display on screen
+label = Label(text='I Am a Label', font=('Arial', 24, 'bold'))
+# label.pack(expand=True)  # display on screen
+label.pack()
 # default to side = 'top'
 # label.pack(side='bottom')
 # label.pack(side='left')
+
+label['text'] = 'new text'
+label.config(text='New text 1')
+
+
+# button
+def button_clicked():
+    print('I got clicked')
+    new_text = input.get()
+    label.config(text=new_text)
+
+
+button = Button(text='Click me', command=button_clicked)
+button.pack()
+
+
+# entry
+input = Entry(width=10)
+input.pack()
 
 
 window.mainloop()
