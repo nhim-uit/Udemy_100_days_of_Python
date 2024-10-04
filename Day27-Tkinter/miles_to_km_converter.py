d26e84b109d0c67f55e563237b849943b902f7e8
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-04 16:14:01 +0700

    Miles to Km Converter

---
+++ Day27-Tkinter/miles_to_km_converter.py
@@ -0,0 +1,43 @@
+# Udemy: Master Python by building 100 projects in 100 days
+# Oct 4, 2024
+# Day 27 - Miles to Km Converter
+# Created by me
+
+from tkinter import *
+
+window = Tk()
+window.title('Miles to Km Converter')
+# window.minsize(width=100, height=100)
+window.config(padx=5, pady=20)
+
+# Entry
+text_box = Entry(window, width=10)
+text_box.grid(column=1, row=0)
+
+# label: miles
+miles_lb = Label(text='Miles', font=('Arial', 12, 'normal'))
+miles_lb.grid(column=2, row=0)
+
+# label: is equal to
+is_equal_to_lb = Label(text='is equal to', font=('Arial', 12, 'normal'))
+is_equal_to_lb.grid(column=0, row=1)
+
+# label: zero
+zero_lb = Label(text='0', font=('Arial', 12, 'normal'))
+zero_lb.grid(column=1, row=1)
+
+# label: km
+km_lb = Label(text='Km', font=('Arial', 12, 'normal'))
+km_lb.grid(column=2, row=1)
+
+
+# button
+def button_click():
+    km = round(int(text_box.get()) * 1.609)
+    zero_lb.config(text=km)
+
+
+button = Button(text='Calculate', command=button_click)
+button.grid(column=1, row=2)
+
+window.mainloop()
