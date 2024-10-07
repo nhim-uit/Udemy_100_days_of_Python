77b31e43449a4555a4b8070c0bbc38c970ff4e48
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-07 16:02:16 +0700

    add title_lb as parameter for pomodoro(), break_time(), start()

--- Day28-PomodoroApp/Timer.py
+++ Day28-PomodoroApp/Timer.py
@@ -12,7 +12,7 @@ class Timer:
         self.text_id = text_id
         self.window = window
 
-    def update_timer(self, check_mark_lb):
+    def update_timer(self, check_mark_lb, title_lb):
         if self.__running:
             self.__second += 1
 
@@ -24,21 +24,21 @@ class Timer:
             self.canvas.itemconfig(self.text_id, text=f'{self.__minute:02}:{self.__second:02}')
 
             if self.__reps % 2 == 0:
-                self.pomodoro()
+                self.pomodoro(title_lb)
 
                 # config label
                 check_mark_lb.config(text=self.__check_mark * '✔')
 
             else:
-                self.break_time()
+                self.break_time(title_lb)
 
             # update after every 1000ms (1 second)
-            self.window.after(1000, self.update_timer, check_mark_lb)
+            self.window.after(1, self.update_timer, check_mark_lb, title_lb)
 
-    def start(self, check_mark_lb):
+    def start(self, check_mark_lb, title_lb):
         if not self.__running:
             self.__running = True
-            self.update_timer(check_mark_lb)
+            self.update_timer(check_mark_lb, title_lb)
 
     def reset(self, canvas, text_id):
         self.__running = False
@@ -46,7 +46,9 @@ class Timer:
         self.__second = 0
         canvas.itemconfig(text_id, text='00:00')
 
-    def pomodoro(self):
+    def pomodoro(self, title_lb):
+        title_lb.config(text='Work', fg=GREEN)
+
         if self.__minute == WORK_MIN and self.__second == 0:
             self.__check_mark += 1
 
@@ -54,8 +56,10 @@ class Timer:
             self.__second = 0
             self.__reps += 1
 
-    def break_time(self):
+    def break_time(self, title_lb):
+        title_lb.config(text='Break', fg=PINK)
         if self.__reps % LONG_BREAK_REPS == 0:
+            title_lb.config(fg=BLUE)
             if self.__minute == LONG_BREAK_MIN and self.__second == 0:
                 self.reset_minute_second()
 
