bbfcdd37497f74639083c6e0e70df7876dbd1c4b
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-07 15:40:50 +0700

    delete CONSTANTS

--- Day28-PomodoroApp/main.py
+++ Day28-PomodoroApp/main.py
@@ -12,17 +12,12 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset():
     timer.reset(canvas, text_id)
 
-
-# ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def start():
