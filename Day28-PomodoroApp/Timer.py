1b77f9e6602a2bbdb7f6dc159c4c8715fcb4c02c
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-07 15:29:41 +0700

    add break time, pomodoro 25 mins

--- Day28-PomodoroApp/Timer.py
+++ Day28-PomodoroApp/Timer.py
@@ -4,6 +4,7 @@ class Timer:
         self.__second = 0
         self.__running = False
         self.__check_mark = 0
+        self.__reps = 0
         self.canvas = canvas
         self.text_id = text_id
         self.window = window
@@ -12,7 +13,7 @@ class Timer:
     def check_mark(self):
         return self.__check_mark
 
-    def update_timer(self):
+    def update_timer(self, check_mark_lb):
         if self.__running:
             self.__second += 1
 
@@ -20,25 +21,49 @@ class Timer:
                 self.__second = 0
                 self.__minute += 1
 
-            # print(f'{self.__minute}:{self.__second}')
-
             # update the text on canvas
             self.canvas.itemconfig(self.text_id, text=f'{self.__minute:02}:{self.__second:02}')
 
-            if self.__minute == 0 and self.__second == 3:
-                self.__check_mark += 1
-                return
+            if self.__reps % 2 == 0:
+                self.pomodoro()
+
+                # config label
+                check_mark_lb.config(text=self.__check_mark * '✔')
+
+            else:
+                self.break_time()
 
             # update after every 1000ms (1 second)
-            self.window.after(1000, self.update_timer)
+            self.window.after(1000, self.update_timer, check_mark_lb)
 
-    def start(self, canvas, text_id, window):
+    def start(self, check_mark_lb):
         if not self.__running:
             self.__running = True
-            self.update_timer()
+            self.update_timer(check_mark_lb)
 
-    def reset(self, canvas, text_id, window):
+    def reset(self, canvas, text_id):
         self.__running = False
         self.__minute = 0
         self.__second = 0
         canvas.itemconfig(text_id, text='00:00')
+
+    def pomodoro(self):
+        if self.__minute == 25 and self.__second == 0:
+            self.__check_mark += 1
+
+            self.__minute = 0
+            self.__second = 0
+            self.__reps += 1
+
+    def break_time(self):
+        if self.__reps % 7 == 0:
+            if self.__minute == 20 and self.__second == 0:
+                self.__minute = 0
+                self.__second = 0
+                self.__reps += 1
+
+        elif self.__minute == 5 and self.__second == 0:
+            self.__minute = 0
+            self.__second = 0
+            self.__reps += 1
+
