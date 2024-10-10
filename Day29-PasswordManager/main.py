3b4be0104930a3533afbe265547fcd33909e90ee
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-10 21:08:36 +0700

    add functions generate_password() and save()

--- Day29-PasswordManager/main.py
+++ Day29-PasswordManager/main.py
@@ -3,9 +3,31 @@
 # Day 29 - Password Manager App with Tkinter
 # Created by me
 
+import random
 from tkinter import *
 
 from CONSTANTS import *
+
+
+# functions
+def generate_password():
+    password_entry.delete(0, END)
+    password_entry.insert(0, str(random.randint(100000, 999999)))
+
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open('data.txt', 'a') as file:
+        file.write(f'{website} | {email} | {password}\n')
+
+    website_entry.delete(0, END)
+    email_entry.delete(0, END)
+    email_entry.insert(0, 'alex@gmail.com')
+    password_entry.delete(0, END)
+
 
 # window
 window = Tk()
@@ -29,15 +51,16 @@ password_lb = Label(text='Password:', font=FONT)
 password_lb.grid(column=0, row=3)
 
 # button
-gen_pass_btn = Button(text='Generate Password')
+gen_pass_btn = Button(text='Generate Password', command=generate_password)
 gen_pass_btn.grid(column=2, row=3, padx=(50, 0))
 
-add_btn = Button(text='Add')
+add_btn = Button(text='Add', command=save)
 add_btn.grid(column=1, row=4, columnspan=2, sticky='ew', pady=2)
 # use sticky='ew' to span all its grid cell
 
 # entry
 website_entry = Entry(window, font=FONT)
+website_entry.focus()
 website_entry.grid(column=1, row=1, columnspan=2, sticky='ew', pady=2)
 
 email_entry = Entry(window, font=FONT)
