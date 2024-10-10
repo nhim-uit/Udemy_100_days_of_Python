1cdd0f10b23a7c26dc5f4cd808121479ae60af41
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-10 21:20:56 +0700

    add messagebox

--- Day29-PasswordManager/main.py
+++ Day29-PasswordManager/main.py
@@ -5,6 +5,7 @@
 
 import random
 from tkinter import *
+from tkinter import messagebox
 
 from CONSTANTS import *
 
@@ -20,13 +21,22 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open('data.txt', 'a') as file:
-        file.write(f'{website} | {email} | {password}\n')
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title='Oops', message='Please don\'t leave any fields empty! ')
+    else:
+        is_ok = messagebox.showinfo(title=website, message=f"These are the details entered:\n"
+                                                           f"Website: {website}\n"
+                                                           f"Email: {email}\n"
+                                                           f"Password: {len(password) * '*'}\n"
+                                                           f"Is it ok to save?")
+        if is_ok:
+            with open('data.txt', 'a') as file:
+                file.write(f'{website} | {email} | {password}\n')
 
-    website_entry.delete(0, END)
-    email_entry.delete(0, END)
-    email_entry.insert(0, 'alex@gmail.com')
-    password_entry.delete(0, END)
+            website_entry.delete(0, END)
+            email_entry.delete(0, END)
+            email_entry.insert(0, 'alex@gmail.com')
+            password_entry.delete(0, END)
 
 
 # window
