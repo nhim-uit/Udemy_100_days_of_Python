c0681badbd00aa129448930d85017c18cb1a3fe9
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-16 11:52:04 +0700

    change content of text2 with random word in every time the buttons are pressed

--- Day31-CapstoneProject-FlashCardProgram/main.py
+++ Day31-CapstoneProject-FlashCardProgram/main.py
@@ -6,18 +6,24 @@
 from tkinter import *
 
 import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 
 # functions
 def get_word():
-    filename = 'data1.csv'
-    df = pandas.read_csv(filename, header=None, index_col=False)
+    filename = 'cleaned_file.csv'
+    df = pandas.read_csv(filename, index_col=False)
     data_dict = df.to_dict(orient='records')
-    print(df)
+    random_word = random.choice(data_dict).get('Vietnamese')
+    canvas.itemconfig(text2, text=random_word)
 
 
+
+
+
+# window
 window = Tk()
 window.title('Flash Card :)')
 window.config(width=1000, height=750, padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -30,7 +36,7 @@ card_font_img = PhotoImage(file='./images/card_front.png')
 # Canvas
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 canvas.create_image(0, 0, anchor='nw', image=card_font_img)
-text1 = canvas.create_text(400, 150, text='French',
+text1 = canvas.create_text(400, 150, text='Vietnamese',
                            font=('Arial', 40, 'italic'), fill='black')
 text2 = canvas.create_text(400, 263, text='word',
                            font=('Arial', 60, 'bold'), fill='black')
@@ -41,7 +47,8 @@ wrong_btn = Button(image=wrong_img, highlightthickness=0,
                    borderwidth=0, command=get_word)
 wrong_btn.grid(column=0, row=1)
 
-right_btn = Button(image=right_img, highlightthickness=0, borderwidth=0)
+right_btn = Button(image=right_img, highlightthickness=0,
+                   borderwidth=0, command=get_word)
 right_btn.grid(column=1, row=1)
 
 window.mainloop()
