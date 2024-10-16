a2f3ea5cb45859b000c04da8fffb46dd9be84872
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-16 13:57:37 +0700

    create example_data.csv

--- Day31-CapstoneProject-FlashCardProgram/clean_csv_file.py
+++ Day31-CapstoneProject-FlashCardProgram/clean_csv_file.py
@@ -9,3 +9,4 @@ df_cleaned = df[~df['Vietnamese'].str.contains('\?', na=False)]
 # print(df_cleaned)
 
 df_cleaned.to_csv('cleaned_file.csv', index=False, encoding='utf-8')
+df_cleaned.head(5).to_csv('example_data.csv', index=False, encoding='utf-8')
