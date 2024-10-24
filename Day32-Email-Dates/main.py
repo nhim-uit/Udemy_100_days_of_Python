49b0f09e95b7aed6e96da128a47c32c5f9d64905
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-24 09:27:51 +0700

    send email

---
+++ Day32-Email-Dates/main.py
@@ -0,0 +1,15 @@
+# Udemy: Master Python by building 100 projects in 100 days
+# Oct 23, 2024
+# Day 32 - Send Emails and Manage Dates
+
+import smtplib
+
+my_email = 'alex.test.app.7@gmail.com'
+my_password = 'dvhuulpnkkpfkwts'
+
+with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=my_password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs='alexisfun065@gmail.com',
+                        msg='Subject:Hello\n\nThis is the body of my email.')
