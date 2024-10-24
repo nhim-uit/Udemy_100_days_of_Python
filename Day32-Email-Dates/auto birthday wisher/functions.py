3029a6287d7a367fe91caa4b92559e23ce9bcb5a
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-24 11:04:11 +0700

    refactor code to functions

---
+++ Day32-Email-Dates/auto birthday wisher/functions.py
@@ -0,0 +1,52 @@
+import datetime as dt
+import random
+from email.mime.text import MIMEText
+import smtplib
+import pandas
+
+
+def get_df(filename):
+    # open file to read
+    try:
+        df = pandas.read_csv(filename)
+    except FileNotFoundError:
+        print(f'{filename} file not found')
+    return df
+
+
+def get_day_month():
+    return dt.datetime.now().day, dt.datetime.now().month
+
+
+def get_letter_content(row):
+    # randomly choose a letter template
+    letter_no = random.randint(1, 3)
+    template_file = f'letter_{letter_no}.txt'
+
+    # read template file and replace name
+    try:
+        with open(template_file, 'r') as template:
+            letter_content = template.read()
+    except FileNotFoundError as e:
+        print(f'{template_file} not found')
+
+    return letter_content.replace('[NAME]', row['name'])
+
+
+def create_email_msg(new_letter, my_email, recipient_email):
+    msg = MIMEText(new_letter, 'plain', 'utf-8')
+    msg['Subject'] = 'Happy Birthday!'
+    msg['From'] = my_email
+    msg['To'] = recipient_email
+    return msg
+
+
+def send_email(my_email, my_password, recipient_email, msg):
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(from_addr=my_email,
+                            to_addrs=recipient_email,
+                            msg=msg.as_string())
+
+    print('Email sent successfully')
--- Day32-Email-Dates/auto birthday wisher/main.py
+++ Day32-Email-Dates/auto birthday wisher/main.py
@@ -3,63 +3,34 @@
 # Day 32 - Automated Birthday Wisher
 # Created by me
 
-import pandas
-import random
-import datetime as dt
-import smtplib
-from email.mime.text import MIMEText
+from functions import *
 
 # email info
 my_email = 'alex.test.app.7@gmail.com'
 my_password = 'dvhuulpnkkpfkwts'
 
 # datetime
-now = dt.datetime.now()
-target_day = now.day
-target_month = now.month
+target_day, target_month = get_day_month()
 
 # list of birthdays
 birthday_list = 'birthday.csv'
-
-# open file to read
-try:
-    df = pandas.read_csv(birthday_list)
-except FileNotFoundError:
-    print(f'{birthday_list} file not found')
+df = get_df(birthday_list)
 
 # find matching birthday
 matching_rows = df[(df['day'] == target_day) & (df['month'] == target_month)]
 
 # iterate through df matching birthdays
 for index, row in matching_rows.iterrows():
-    # randomly choose a letter template
-    letter_no = random.randint(1, 3)
-    template_file = f'letter_{letter_no}.txt'
-    print(template_file)
+    new_letter = get_letter_content(row)
 
-    # read template file and replace name
-    try:
-        with open(template_file, 'r') as template:
-            letter_content = template.read()
-    except FileNotFoundError as e:
-        print(f'{template_file} not found')
-
-    new_letter = letter_content.replace('[NAME]', row['name'])
-    # print(new_letter)
-
+    # get recipient email
     recipient_email = row['email']
 
     # create the email message
-    msg = MIMEText(new_letter, 'plain', 'utf-8')
-    msg['Subject'] = 'Happy Birthday!'
-    msg['From'] = my_email
-    msg['To'] = recipient_email
+    msg = create_email_msg(new_letter, my_email, recipient_email)
 
-    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
-        connection.starttls()
-        connection.login(user=my_email, password=my_password)
-        connection.sendmail(from_addr=my_email,
-                            to_addrs=recipient_email,
-                            msg=msg.as_string())
+    # send email
+    send_email(my_email, my_password, recipient_email, msg)
 
-    print('Email sent successfully')
+
+
