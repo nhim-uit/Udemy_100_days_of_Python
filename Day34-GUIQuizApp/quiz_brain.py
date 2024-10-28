38d130606b461f243f7cb687923cef839325045a
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-10-28 17:37:10 +0700

    quiabrain class

---
+++ Day34-GUIQuizApp/quiz_brain.py
@@ -0,0 +1,52 @@
+from question import *
+
+
+class QuizBrain:
+    def __init__(self):
+        self.__question_number = 0
+        self.__question_list = QuestionBank()
+        self.__score = 0
+
+    @property
+    def question_number(self):
+        return self.__question_number
+
+    @property
+    def question(self):
+        self.__question_list.number = self.__question_number
+        return self.__question_list.question_bank_question
+
+    @property
+    def question_list(self):
+        return self.__question_list.question_bank
+
+    def update_question(self):
+        self.__question_number += 1
+
+    def update_score(self):
+        self.__score += 1
+
+    def next_question(self):
+        q = self.question
+        self.update_question()
+        user_ans = input(f'Q.{self.__question_number}: {q.question} (True/False)?: ')
+        self.check_answer(user_ans, q.answer)
+
+    def still_has_questions(self):
+        return self.__question_number < self.__question_list.len
+
+    def check_answer(self, user_ans, correct_ans):
+        if user_ans.lower() == correct_ans.lower():
+            self.update_score()
+            print('You got it right!')
+        else:
+            print('That\'s wrong.')
+
+        print(f'The correct answer was: {correct_ans}.')
+        print(f'Your current score is: {self.__score}/{self.__question_number}.\n')
+
+    def complete(self):
+        print('*' * 27)
+        print('You\'ve completed the quiz.')
+        print(f'Your final score was: {self.__score}/{self.__question_number}.')
+        print('*' * 27)
