bb02ba797713bde457d95b2eb468d8ceafba8590
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-11-11 17:34:00 +0700

    Update main.py

--- Day35-StockTradingNewsAlert/main.py
+++ Day35-StockTradingNewsAlert/main.py
@@ -3,3 +3,15 @@
 # Day 36 - Stock Trading News Alert
 # Created by me
 
+from stock_price import *
+
+if __name__ == '__main__':
+    stock = 'AAPL'  # Apple, Inc. stock
+
+    prev_price = get_prev_closing_price(stock)
+    today_price = get_today_closing_price(stock)
+
+    change = calc_percent(prev_price, today_price)
+
+    print(f'change: {change}%')
+    print(f'{get_news(stock)}')
