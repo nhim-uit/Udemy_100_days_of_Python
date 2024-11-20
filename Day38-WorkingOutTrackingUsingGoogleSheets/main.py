d8a968e21c55f3372556ae0e2568ad3d90113af8
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-11-20 21:21:52 +0700

    sheety api to post data to google sheets

--- Day38-WorkingOutTrackingUsingGoogleSheets/main.py
+++ Day38-WorkingOutTrackingUsingGoogleSheets/main.py
@@ -1,9 +1,10 @@
 # Udemy: Master Python by building 100 projects in 100 days
-# Nov 19, 2024
+# Nov 20, 2024
 # Day 38 - Working Out Tracking Using Google Sheets
 
 import os
 import requests
+from datetime import datetime
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -30,11 +31,31 @@ nutritionix_params = {
 
 responses = requests.post(url=nutritionix_endpoint, json=nutritionix_params, headers=nutritionix_headers)
 result = responses.json()
-print(result['exercises'][0]['name'])
-print(result['exercises'][0]['duration_min'])
-print(result['exercises'][0]['nf_calories'])
+print(result)
 
+# SHEETY API
+SHEETY_API_KEY = os.getenv('SHEETY_API_KEY')
 
+sheety_endpoint = f'https://api.sheety.co/{SHEETY_API_KEY}/copyOfMyWorkouts/workouts'
 
+today = datetime.now().date().strftime('%d/%m/%Y')
+current_time = datetime.now().time().strftime('%H:%M:%S')
+# print(current_time)
 
+sheety_headers = {
+    'Authorization': os.getenv('SHEETY_HEADER'),
+}
 
+for exercise in result['exercises']:
+    sheety_params = {
+        'workout': {
+            'date': today,
+            'time': current_time,
+            'exercise': exercise['name'].title(),
+            'duration': exercise['duration_min'],
+            'calories': exercise['nf_calories'],
+        }
+    }
+
+    sheety_response = requests.post(url=sheety_endpoint, headers=sheety_headers, json=sheety_params)
+    print(sheety_response.text)
