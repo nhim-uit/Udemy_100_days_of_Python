d00c69bf0bb41706d9cb04366da4cb6dc93e1f21
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-11-24 15:57:57 +0700

    Create main.py

---
+++ Day39-CapstoneFlightDealFinder/main.py
@@ -0,0 +1,9 @@
+# Udemy: Master Python by building 100 projects in 100 days
+# Nov 21, 2024
+# Day 39 - Capstone Project: Flight Deal Finder
+# APIs used:
+# Amadeus Flight Search API: https://developers.amadeus.com/
+# Sheety API
+# Twilio Messaging API
+# Cannot sign up for Amadeus Flight Search API
+
