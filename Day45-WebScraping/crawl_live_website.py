4ce1ff6817da181919c92b9cd93f3fbf5efad597
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-12-12 19:14:02 +0700

    find the max upvote article

--- Day45-WebScraping/crawl_live_website.py
+++ Day45-WebScraping/crawl_live_website.py
@@ -15,3 +15,16 @@ soup = BeautifulSoup(response.text, 'html.parser')
 articles_texts = [tag.select_one(selector='a').getText() for tag in soup.select('.titleline')]
 articles_links = [tag.select_one(selector='a').get('href') for tag in soup.select('.titleline')]
 articles_upvote = [s.getText() for s in soup.find_all(name='span', class_='score')]
+# print(articles_upvote)
+
+upvote = list(filter(None, [int(s.split()[0]) if 'points' in s else None for s in articles_upvote]))
+# print(upvote)
+
+# Zip the lists into a list of dictionaries
+articles = [{'text': text, 'link': link, 'upvote': up}\
+            for text, link, up in zip(articles_texts, articles_links, upvote)]
+
+# Find the article with the maximum upvote
+max_upvote_article = max(articles, key=lambda x: x['upvote'])
+
+print(max_upvote_article)
