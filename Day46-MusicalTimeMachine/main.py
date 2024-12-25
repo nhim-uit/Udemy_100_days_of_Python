6e81b1cffaa6b47498495ec6a164ac8c8c57da74
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-12-25 15:08:03 +0700

    Update main.py

--- Day46-MusicalTimeMachine/main.py
+++ Day46-MusicalTimeMachine/main.py
@@ -1,27 +1,29 @@
 # Udemy: Master Python by building 100 projects in 100 days
 # Dec 24, 2024
 # Day 46 - Create a Spotify Playlist using the Musical Time Machine
-
-import requests
-from bs4 import BeautifulSoup
-
-
-def get_100_songs(date_):
-    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) "
-                            "Gecko/20100101 Firefox/131.0"}
-    url = f'https://www.billboard.com/charts/hot-100/{date_}/'
-    response = requests.get(url=url, headers=header)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    heading = soup.select('li ul li h3')
-
-    titles = [head.getText().strip() for head in heading]
-    # print(titles)
-
-    return titles
-
+from functions import get_100_songs, get_Spotify, create_playlist
 
 if __name__ == '__main__':
+    # Prompt the user to input a date in the format YYYY-MM-DD
     date = input('Which year do you want to travel to? '
              'Type the date in this format YYYY-MM-DD: ')
-    print(get_100_songs(date))
+    # Fetch the top 100 songs from the Billboard Hot 100 chart for the given date
+    list_of_songs = get_100_songs(date)
+
+    songs_uris = []
+    year = date.split('-')[0]
+
+    # Authenticate and get a Spotify client
+    sp = get_Spotify()
+
+    # Search Spotify for each song by title and year, and collect their URIs
+    for title in list_of_songs:
+        results = sp.search(q=f"track:{title} year:{year}", type="track")
+        try:
+            songs_uris.append(results['tracks']['items'][0]['uri'])
+            # print(songs_uris)
+        except IndexError:
+            print('Track cannot be found')
+
+    # Create a new private playlist on Spotify and add the collected songs to it
+    create_playlist(sp, date, songs_uris)
