b88a3b69fcea1b747e91e9800ab52d5a7b9a2bc0
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2024-12-25 15:48:33 +0700

    get price

--- Day47-AutomatedAmazonPriceTracker/functions.py
+++ Day47-AutomatedAmazonPriceTracker/functions.py
@@ -10,3 +10,13 @@ def get_response(url):
     response = requests.get(url)
     return response.text
 
+
+def get_price(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    price_whole = soup.find(name='span', class_='a-price-whole').getText()
+    price_fraction = soup.find(name='span', class_='a-price-fraction').getText()
+    price = float(price_whole + price_fraction)
+
+    return price
+
+
