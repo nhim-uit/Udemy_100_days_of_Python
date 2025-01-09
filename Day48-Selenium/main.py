416d241678488dc16f0f9e41139019c885be123b
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-01-09 14:59:21 +0700

    get button size

---
+++ Day48-Selenium/main.py
@@ -0,0 +1,38 @@
+# Udemy: Master Python by building 100 projects in 100 days
+# Dec 27, 2024
+# Day 48 - Selenium Webdriver browser
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# Keep Chrome browser open after the program finishes
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options=chrome_options)
+# driver.get('https://www.amazon.com/dp/B075CYMYK6?ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6&th=1')
+
+# driver.close() # quit a single tab
+# driver.quit()  # quit the entire Chrome - multiple tabs
+
+# try:
+#     price_dollar = driver.find_element(By.CLASS_NAME, value='a-price-whole')
+#     price_cents = driver.find_element(By.CLASS_NAME, value='a-price-fraction')
+#     print(f'The price is {price_dollar.text}.{price_cents.text}')
+#
+#     driver.close()
+# except Exception as e:
+#     print(f'error {e}')
+
+# search bar for
+driver.get('https://www.python.org')
+
+try:
+    search_bar = driver.find_element(By.NAME, value='q')
+    print(search_bar.get_attribute('placeholder'))
+    button = driver.find_element(By.ID, value='submit')
+    print(button.size)
+except Exception as e:
+    print(e)
+
+driver.close()
