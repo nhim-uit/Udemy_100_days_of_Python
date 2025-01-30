7c301fbb36dadaa81225c0f4afc5572c378c4586
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-01-30 22:03:18 +0700

    Create CONSTANTS.py

---
+++ Day48-Selenium/cookie-playing-bot/CONSTANTS.py
@@ -0,0 +1,6 @@
+SECONDS = 5
+MINUTES = 1
+SECOND_PER_MINUTE = 60
+STORE_IDS = ['buyCursor', 'buyGrandma', 'buyFactory',
+                 'buyMine', 'buyShipment', 'buyAlchemy lab',
+                 'buyPortal', 'buyTime machine', 'buyElder Pledge']
