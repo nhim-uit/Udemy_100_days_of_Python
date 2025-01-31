efe950a8f95c200f1dadf88474ab801d078e108e
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-01-31 17:36:52 +0700

    fix v is none

--- Day48-Selenium/cookie-playing-bot/functions.py
+++ Day48-Selenium/cookie-playing-bot/functions.py
@@ -1,6 +1,6 @@
 import time
 import re
-from CONSTANTS import STORE_IDS
+from CONSTANTS import STORE_IDS, SECONDS
 from selenium.webdriver.common.by import By
 
 
@@ -9,7 +9,8 @@ def click_cookie(cookie, end_time):
         cookie.click()
 
 
-def fill_available_element(available_element, driver):
+def fill_available_element(driver):
+    available_element = {}
     for store_id in STORE_IDS:
         element = driver.find_element(By.ID, value=store_id)
         class_attribute = element.get_attribute('class')
@@ -31,34 +32,46 @@ def get_key_has_largest_value(available_element):
     return k
 
 
-def autobuy(driver):
-    available_element = fill_available_element({}, driver)
-    k = get_key_has_largest_value(available_element)
+def autobuy(driver, end_time):
+    available_element = fill_available_element(driver)
+    try:
+        k = get_key_has_largest_value(available_element)
+        v = available_element.get(k)
+    except:
+        pass
+
     money = int(driver.find_element(By.ID, value='money').text)
 
-    while money >= available_element.get(k):
+    while time.time() < end_time:
         print(f'k = {k}')
         el = driver.find_element(By.ID, value=k)
-        el.click()
+
+        try:
+            if money >= v:
+                el.click()
+        except:
+            pass
 
         print(f'money = {money}')
         print(available_element)
 
         try:
-            available_element = fill_available_element(available_element, driver)
+            available_element = fill_available_element(driver)
             print('---', available_element)
         except:
             print('$available element$')
 
         try:
             k = get_key_has_largest_value(available_element)
+            v = available_element.get(k)
         except:
-            print('$key largest value$')
+            print('$no key largest value$')
 
         money = int(driver.find_element(By.ID, value='money').text)
 
 
 def run(driver, end_time):
     while time.time() < end_time:
-        time.sleep(5)
-        autobuy(driver)
+        time.sleep(SECONDS)
+        autobuy(driver, end_time)
+    print('==============END=============')
