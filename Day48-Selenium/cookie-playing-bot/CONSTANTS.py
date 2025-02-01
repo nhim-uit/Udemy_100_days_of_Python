2c22d7a28926bcae1783ccca5f61b368a47c20da
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-02-01 13:14:24 +0700

    reformat

--- Day48-Selenium/cookie-playing-bot/CONSTANTS.py
+++ Day48-Selenium/cookie-playing-bot/CONSTANTS.py
@@ -2,5 +2,5 @@ SECONDS = 45
 MINUTES = 12
 SECOND_PER_MINUTE = 60
 STORE_IDS = ['buyCursor', 'buyGrandma', 'buyFactory',
-                 'buyMine', 'buyShipment', 'buyAlchemy lab',
-                 'buyPortal', 'buyTime machine', 'buyElder Pledge']
+             'buyMine', 'buyShipment', 'buyAlchemy lab',
+             'buyPortal', 'buyTime machine', 'buyElder Pledge']
