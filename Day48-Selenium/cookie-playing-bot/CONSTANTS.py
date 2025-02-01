32cb099a3f15c264a43e221799cd9e119821fa41
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-02-01 12:22:02 +0700

    test

--- Day48-Selenium/cookie-playing-bot/CONSTANTS.py
+++ Day48-Selenium/cookie-playing-bot/CONSTANTS.py
@@ -1,5 +1,5 @@
-SECONDS = 30
-MINUTES = 7
+SECONDS = 45
+MINUTES = 12
 SECOND_PER_MINUTE = 60
 STORE_IDS = ['buyCursor', 'buyGrandma', 'buyFactory',
                  'buyMine', 'buyShipment', 'buyAlchemy lab',
