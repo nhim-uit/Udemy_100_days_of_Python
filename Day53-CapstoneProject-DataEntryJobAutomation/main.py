1d483a4f9c0755775aa6e390af7769d2d0b08580
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-02-07 21:16:16 +0700

    autofill google form, with waiting time for clickable elements

--- Day53-CapstoneProject-DataEntryJobAutomation/main.py
+++ Day53-CapstoneProject-DataEntryJobAutomation/main.py
@@ -3,13 +3,15 @@
 # Day 53 - Capstone Project: Data Entry Job Automation
 # Created by me (Alex Mai)
 import re
+import time
 
 from bs4 import BeautifulSoup
 import requests
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # crawl data for address, price and link from Zillow
 response = requests.get('https://appbrewery.github.io/Zillow-Clone/')
@@ -24,6 +26,28 @@ prices = [re.sub(r'[^\d,$]', '',
 links = [tag.get('href')
          for tag in soup.select('.StyledPropertyCardDataArea-anchor')]
 
+# Auto fill in google form for address, price and link
+chrome_option = webdriver.ChromeOptions()
+chrome_option.add_experimental_option('detach', True)
+
+driver = webdriver.Chrome(options=chrome_option)
 
 
 
+for i in range(len(addresses)):
+    driver.get('https://forms.gle/t37jcURPRFmpxEH39')
+
+    wait = WebDriverWait(driver, 10)
+
+    input_address = wait.until(EC.element_to_be_clickable(driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+    input_price = wait.until(EC.element_to_be_clickable(driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+    input_link = wait.until(EC.element_to_be_clickable(driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')))
+
+    input_address.send_keys(addresses[i])
+    input_price.send_keys(prices[i])
+    input_link.send_keys(links[i])
+
+    button = wait.until(EC.element_to_be_clickable(driver.find_element(By.XPATH, '/html/body/div/div[2]/form/div[2]/div/div[3]/div[1]/div[1]/div')))
+    button.click()
+
+driver.close()
