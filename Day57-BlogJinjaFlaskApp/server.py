9fc445fe65e2c87d68afb06910199d745bc95727
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-02-12 15:57:37 +0700

    reorganize files

--- Day57-BlogJinjaFlaskApp/server.py
+++ Day57-BlogJinjaFlaskApp/server.py
@@ -1,13 +1,20 @@
 # Udemy: Master Python by building 100 projects in 100 days
 # Feb 11, 2025
-# Day 57 - Age and gender guessing using API agify and genderize, Flask, Jinja
-# Created by me (Alex Mai)
+# Day 57 - Templating with Jinja in Flask Application
 
 from flask import Flask, render_template
+import random
+import datetime
 import requests
 
-
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    random_num = random.randint(1, 10)
+    year = datetime.datetime.now().year
+    return render_template('index.html', num=random_num, year=year)
 
 
 @app.route('/<name>')
@@ -21,5 +28,16 @@ def get_results(name):
     return render_template('guess.html', name=name.title(), gender=gender, age=age)
 
 
+@app.route('/blog')
+def get_blog():
+    blog_url = 'https://api.npoint.io/c790b4d5cab58020d391'
+
+    response = requests.get(blog_url)
+    all_posts = response.json()
+
+    return render_template('blog.html', posts=all_posts)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
