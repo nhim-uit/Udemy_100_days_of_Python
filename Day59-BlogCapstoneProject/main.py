66edb13377cf7c1dde2a1ffaeb6e52a0f37b029a
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-02-19 21:29:27 +0700

    add redirect link to about and contact

--- Day59-BlogCapstoneProject/main.py
+++ Day59-BlogCapstoneProject/main.py
@@ -13,5 +13,15 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/about')
+def get_about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def get_contact():
+    return render_template('contact.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
