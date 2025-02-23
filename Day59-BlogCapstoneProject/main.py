431291d978b112b5aea149fcdf943c1197f0544f
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-02-23 19:38:03 +0700

    add request blog info from api npoint

--- Day59-BlogCapstoneProject/main.py
+++ Day59-BlogCapstoneProject/main.py
@@ -3,14 +3,35 @@
 # Add upgrade to existing blog template
 # Created by me
 
+import requests
 from flask import Flask, render_template
+from streamlit import title
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    response = requests.get('https://api.npoint.io/5ab7588e98376bb9d4dc').json()
+    titles = []
+    subtitles = []
+    authors = []
+    dates = []
+
+    for i in response:
+        titles.append(i['title'])
+        subtitles.append(i['subtitle'])
+        authors.append(i['author'])
+        dates.append(i['date'])
+
+    length = len(titles)
+
+    return render_template('index.html',
+                           titles=titles,
+                           subtitles=subtitles,
+                           authors=authors,
+                           dates=dates,
+                           length=length)
 
 
 @app.route('/about')
