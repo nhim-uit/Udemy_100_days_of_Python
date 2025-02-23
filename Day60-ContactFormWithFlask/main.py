76d7d5ba1a4c4d0cae70c076ea9f1f61b7a641c1
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-02-23 20:48:06 +0700

    Update main.py

--- Day60-ContactFormWithFlask/main.py
+++ Day60-ContactFormWithFlask/main.py
@@ -1,3 +1,16 @@
 # Udemy: Master Python by building 100 projects in 100 days
 # Feb 19-23, 2025
 # Day 60 - Contact Form with POST and Flask
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
