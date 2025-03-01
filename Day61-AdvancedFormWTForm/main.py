5175730d448c4689fb52a723a47774c33f0dc25c
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-03-01 14:40:08 +0700

    added redirect url

--- Day61-AdvancedFormWTForm/main.py
+++ Day61-AdvancedFormWTForm/main.py
@@ -2,7 +2,8 @@
 # Feb 26 - Mar 01, 2025
 # Day 61 - Advanced form with WTForms (Flask)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import EmailField, PasswordField
 from wtforms.fields.simple import SubmitField
@@ -10,6 +11,8 @@ from wtforms.validators import DataRequired, Email, Length
 
 app = Flask(__name__)
 app.secret_key = 'asdf213'
+
+bootstrap = Bootstrap5(app)
 
 
 class LoginForm(FlaskForm):
@@ -26,9 +29,20 @@ def home():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     login_form = LoginForm()
-    login_form.validate_on_submit()
+    if login_form.validate_on_submit():
+        return redirect(url_for('success'))
+
     return render_template('login.html', form=login_form)
 
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+
+@app.route('/denied')
+def denied():
+    return render_template('denied.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
