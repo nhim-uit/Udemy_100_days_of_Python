e8f60c6da6ddedb5700be1a1657ae10d079a5dec
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-03-02 12:36:22 +0700

    use sqlalchemy

--- Day63-VirtualBookshelf/main.py
+++ Day63-VirtualBookshelf/main.py
@@ -5,24 +5,46 @@
 
 from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
+from sqlalchemy import Integer, Float
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from wtforms.fields import StringField, SubmitField, FloatField
 from wtforms.validators import DataRequired
-import sqlite3
+# import sqlite3
 
-db = sqlite3.connect('books-collection.db', check_same_thread=False)
-cursor = db.cursor()
+# db = sqlite3.connect('books-collection.db', check_same_thread=False)
+# cursor = db.cursor()
+#
+# cursor.execute('CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, '
+#                'title varchar(250) NOT NULL UNIQUE, '
+#                'author varchar(250) NOT NULL, rating FLOAT NOT NULL)')
 
-cursor.execute('CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY, '
-               'title varchar(250) NOT NULL UNIQUE, '
-               'author varchar(250) NOT NULL, rating FLOAT NOT NULL)')
+
+class Base(DeclarativeBase):
+    pass
 
 
+db = SQLAlchemy(model_class=Base)
+
+# create app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abcde'
 Bootstrap5(app)
 
+# configure the SQLite db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+# initialize the app
+db.init_app(app)
+
 # all_books = []
+
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True)
+    author: Mapped[str] = mapped_column(nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
 
 
 class BookForm(FlaskForm):
@@ -30,6 +52,11 @@ class BookForm(FlaskForm):
     author = StringField('Book Author', validators=[DataRequired()])
     rating = FloatField('Rating', validators=[DataRequired()])
     submit = SubmitField('Add Book', validators=[DataRequired()])
+
+
+# create table schema in the db
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -42,14 +69,24 @@ def add():
     form = BookForm()
 
     if form.validate_on_submit():
+        # using dictionary
         # all_books.append({
         #     'title': form.name.data,
         #     'author': form.author.data,
         #     'rating': form.rating.data,
         # })
-        cursor.execute('INSERT INTO books (title, author, rating) VALUES (?, ?, ?)',
-                       (form.name.data, form.author.data, form.rating.data))
-        db.commit()
+
+        # using sqlite db
+        # cursor.execute('INSERT INTO books (title, author, rating) VALUES (?, ?, ?)',
+        #                (form.name.data, form.author.data, form.rating.data))
+        # db.commit()
+        book = Book(
+            title=form.name.data,
+            author=form.author.data,
+            rating=form.rating.data,
+        )
+        db.session.add(book)
+        db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html', form=form)
 
