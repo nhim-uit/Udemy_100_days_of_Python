c4d379e2acada127cf5036ac3d4dc55debe6da70
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-03-03 21:13:33 +0700

    add db and add()

--- Day64-Top10Movies/main.py
+++ Day64-Top10Movies/main.py
@@ -1,18 +1,69 @@
 # Udemy: Master Python by building 100 projects in 100 days
 # Mar 02, 2025
 # Day 64 - My Top 10 Movies
-# Created by me (Alex Mai)
+# Completed by me (Alex Mai)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Integer, Float
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 
+
+class Base(DeclarativeBase):
+    pass
+
+
+# Database
+db = SQLAlchemy(model_class=Base)
+
+
+# Create app
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'abcde(*&*%$JGJKYJK9876'
 Bootstrap5(app)
+
+# configure the SQLite db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
+# initialize the app
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(nullable=False)
+    img_url: Mapped[str] = mapped_column(nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/add')
+def add():
+    new_movie = Movie(
+        title='Phone Both',
+        year=220,
+        description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+        rating=7.3,
+        ranking=10,
+        review="My favourite character was the caller.",
+        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg",
+    )
+    
+    db.session.add(new_movie)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
