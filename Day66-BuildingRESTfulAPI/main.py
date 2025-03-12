d46bc9584566769f8ba75287715f24234a119ce2
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-03-13 00:00:40 +0700

    added to_dict method

--- Day66-BuildingRESTfulAPI/main.py
+++ Day66-BuildingRESTfulAPI/main.py
@@ -49,6 +49,9 @@ class Cafe(db.Model):
     can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
     coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
 
 with app.app_context():
     db.create_all()
@@ -65,20 +68,20 @@ def get_random_cafe():
     res = db.session.execute(db.select(Cafe))
     all_cafes = res.scalars().all()
     random_cafe = random.choice(all_cafes)
-    return jsonify(cafe={
-        'id': random_cafe.id,
-        'name': random_cafe.name,
-        'map_url': random_cafe.map_url,
-        'img_url': random_cafe.img_url,
-        'location': random_cafe.location,
-        'seats': random_cafe.seats,
-        'has_toilet': random_cafe.has_toilet,
-        'has_wifi': random_cafe.has_wifi,
-        'has_sockets': random_cafe.has_sockets,
-        'can_take_calls': random_cafe.can_take_calls,
-        'coffee_price': random_cafe.coffee_price,
-    })
-
+    # return jsonify(cafe={
+    #     'id': random_cafe.id,
+    #     'name': random_cafe.name,
+    #     'map_url': random_cafe.map_url,
+    #     'img_url': random_cafe.img_url,
+    #     'location': random_cafe.location,
+    #     'seats': random_cafe.seats,
+    #     'has_toilet': random_cafe.has_toilet,
+    #     'has_wifi': random_cafe.has_wifi,
+    #     'has_sockets': random_cafe.has_sockets,
+    #     'can_take_calls': random_cafe.can_take_calls,
+    #     'coffee_price': random_cafe.coffee_price,
+    # })
+    return jsonify(cafe=random_cafe.to_dict())
 
 # HTTP POST - Create Record
 
