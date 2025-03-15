57c646a07eba10d6cd8e22154c45057aecdc1d7b
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-03-15 13:57:10 +0700

    added search for cafes at a location

--- Day66-BuildingRESTfulAPI/main.py
+++ Day66-BuildingRESTfulAPI/main.py
@@ -92,6 +92,18 @@ def get_all_cafes():
     return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
 
 
+@app.route('/search')
+def get_cafe_location():
+    location = request.args.get('loc')
+    res = db.session.execute(db.select(Cafe).where(Cafe.location == location))
+    all_cafes = res.scalars().all()
+
+    if all_cafes:
+        return jsonify(cafes=[cafe.to_dict() for cafe in all_cafes])
+    else:
+        return jsonify(error={'Not Found': 'Sorry, we don\'t have a cafe at that location.'}), 404
+
+
 # HTTP POST - Create Record
 
 # HTTP PUT/PATCH - Update Record
