81f9afabd53f2ae7ad2795500b5eccf3733c3d2b
nhim-21730055 <165756103+nhim-uit@users.noreply.github.com>
2025-03-29 10:17:36 +0700

    feat: add check hash password to log in user

--- Day68-Authentication/main.py
+++ Day68-Authentication/main.py
@@ -1,18 +1,19 @@
+from wsgiref.util import request_uri
+
 import werkzeug
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, abort
 from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, Boolean
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SECRET_KEY'] = 'befeb4f249f52574ae2592c0c2f193420026fc8004ab66c654edd6255778a4f6'
+
 
 # CREATE DATABASE
-
-
 class Base(DeclarativeBase):
     pass
 
@@ -21,11 +22,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
+
+# Login Manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 # CREATE TABLE IN DB
-
-
-class User(db.Model):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+class User(db.Model, UserMixin):
+    id: Mapped[str] = mapped_column(String(10), primary_key=True)
     email: Mapped[str] = mapped_column(String(100), unique=True)
     password: Mapped[str] = mapped_column(String(100))
     name: Mapped[str] = mapped_column(String(1000))
@@ -45,19 +50,32 @@ def register():
     if request.method == 'POST':
         new_user = User(
             email=request.form.get('email'),
-            password=werkzeug.security.generate_password_hash(request.form.get('password'), method='pbkdf2', salt_length=8),
+            password=werkzeug.security.generate_password_hash(
+                request.form.get('password'),
+                method='pbkdf2',
+                salt_length=8),
             name=request.form.get('name')
         )
 
         db.session.add(new_user)
         db.session.commit()
-        return render_template('secrets.html', name=request.form.get('name'))
+        return render_template('secrets.html', user=new_user)
 
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        res = db.session.execute(db.select(User).where(User.email == email))
+        user = res.scalar()
+
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('secrets'))
+
     return render_template("login.html")
 
 
